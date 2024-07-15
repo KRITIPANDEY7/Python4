60abec922b9c35b65bfed92e2c3a28aa98ba49bd
KRITIPANDEY7 <kritip423@gmail.com>
2024-07-15 15:15:12 +0530

    Delete pythonproject4.py

--- pythonproject4.py
+++
@@ -1,7 +0,0 @@
-import random 
-chars ="abcdefghijklmnopqrstuvwcyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#_*()[]{}"
-length = int(input("Enter length : " ))
-password = "  "
-for a in range(length):
-	password += random.choice(chars)
-	print(password)
