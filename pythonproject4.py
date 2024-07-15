18faf46eb5f05c494fd1d1f7abc3c4a5c80a2fcc
KRITIPANDEY7 <kritip423@gmail.com>
2024-07-15 16:01:26 +0530

    pythonproject4.py

--- pythonproject4.py
+++ pythonproject4.py
@@ -1,5 +1,7 @@
 import random 
+# enter the lowercase ,uppercase,numbers,and symbols 
 chars ="abcdefghijklmnopqrstuvwcyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#_*()[]{}"
+# enter the length
 length = int(input("Enter length : " ))
 password = "  "
 for a in range(length):
